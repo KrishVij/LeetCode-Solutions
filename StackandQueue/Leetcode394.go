e948df9b1debb45b2b9d870f7bdef99151dcd769
KrishVij <official.krishvij@gmail.com>
2025-06-09 13:29:32 +0530

    Published on 09/06/25

--- StackandQueue/Leetcode394.go
+++ StackandQueue/Leetcode394.go
@@ -1,48 +1,45 @@
 package main
 
-import "fmt"
-
-func Push(arr *[]int, x int) {
-
-	*arr = append(*arr, x)
-
-}
-
-func Remove(arr *[]int) {
-
-	if len(*arr) == 0 {
-
-		return
-	}
-
-	*arr = (*arr)[:len(*arr)-1]
-}
-
-func printTop(arr []int) int {
-
-	if len(arr) == 0 {
-
-		return -1
-	}
-	return arr[len(arr)-1]
-}
-
-func Size(arr []int) int {
-
-	if len(arr) == 0 {
-
-		return 0
-	}
-
-	return len(arr)
-}
+import (
+	"strings"
+	"unicode"
+)
 
 func decodeString(s string) string {
 
-	if len(s) == 0 {
-
-		return ""
-	}
-
+	numStack := []int{}
+	alphaStack := []string{}
 	
+	currentNum := 0
+	Result := ""
+
+	for _,Char := range s {
+
+		if unicode.IsDigit(Char) {
+
+			currentNum = currentNum * 10 + int(Char - '0')
+		}else if Char == '[' {
+
+			numStack = append(numStack, currentNum)
+			alphaStack = append(alphaStack, Result)
+
+			currentNum = 0
+			Result = ""
+		}else if Char == ']' {
+
+			previousNum := numStack[len(numStack) - 1]
+			numStack = numStack[:len(numStack) - 1]
+
+			previousString := alphaStack[len(alphaStack) - 1]
+			alphaStack = alphaStack[:len(alphaStack) - 1]
+
+			Result = previousString + strings.Repeat(Result,previousNum)
+		}else {
+
+			Result += string(Char)
+		}
+		
+	} 
+
+	return Result
 }
